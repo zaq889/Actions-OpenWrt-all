aff74092b642d5c8c5975529540dcb0847fec64a
zaq889 <60058038+zaq889@users.noreply.github.com>
2024-04-22 11:42:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,8 +20,9 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 sed -i '1i src-git kenzo https://github.com/zaq889/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/zaq889/small' feeds.conf.default
 ./scripts/feeds update -a
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*}
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,store,taskd,xterm,quickstart}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 ./scripts/feeds install -a 
+openwrt-21.02
