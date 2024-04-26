1b192768da8a5c576c47d936e656da6c71c46035
zaq889 <60058038+zaq889@users.noreply.github.com>
2024-04-26 15:49:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,7 +20,7 @@
 # git clone https://github.com/ouyangzq/sendat package/sendat
 
 sed -i '1i src-git kenzo https://github.com/zaq889/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/zaq889/small-package' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small-package' feeds.conf.default
 ./scripts/feeds update -a
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,store,taskd,xterm,quickstart}
 rm -rf feeds/packages/utils/v2dat
