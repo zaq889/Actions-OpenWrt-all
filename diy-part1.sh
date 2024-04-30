7989d72de0a49239d153fc93419132c53bc18a05
zaq889 <60058038+zaq889@users.noreply.github.com>
2024-04-30 23:33:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,17 +14,12 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-# git clone https://github.com/ouyangzq/luci-app-cpe.git package/luci-app-cpe
-# git clone https://github.com/ouyangzq/sendat package/sendat
-
-sed -i '1i src-git kenzo https://github.com/zaq889/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/zaq889/small-package' feeds.conf.default
-./scripts/feeds update -a
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,store,taskd,xterm,quickstart}
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-./scripts/feeds update -a
 ./scripts/feeds install -a 
+git clone https://github.com/zaq889/5G-Modem-Support.git feeds/packages
