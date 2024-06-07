dede4d15cb64c6db7d15e1ff339bba2f7d3722c6
zaq889 <60058038+zaq889@users.noreply.github.com>
2024-06-08 02:22:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,6 +23,10 @@ sed -i 's/ImmortalWrt/zaq889/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt-2.4G/my mobile XR30/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i 's/ImmortalWrt-5G/my mobile number is XR30/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
-#修改闪存为256M版本(这是针对原厂128闪存来的，但又要编译256M固件来的）
+#修改闪存为512M版本(这是针对原厂128闪存来的，但又要编译512M固件来的）
 sed -i 's/<0x580000 0x7200000>/<0x580000 0x1e000000>/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dts
 sed -i 's/116736k/480m/g' target/linux/mediatek/image/mt7981.mk
+sed -i 's/<0x580000 0x6e80000>/<0x580000 0x1e000000>/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dts
+sed -i 's/114816k/480m/g' target/linux/mediatek/image/mt7981.mk
+
+
