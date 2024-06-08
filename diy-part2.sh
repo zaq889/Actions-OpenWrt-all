875f733863173b274fd1e7f908ee506e14bfd39d
zaq889 <60058038+zaq889@users.noreply.github.com>
2024-06-08 13:52:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,7 +26,7 @@ sed -i 's/ImmortalWrt-5G/my mobile number is XR30/g' package/mtk/applications/mt
 #修改闪存为512M版本(这是针对原厂128闪存来的，但又要编译512M固件来的）
 sed -i 's/<0x580000 0x7200000>/<0x580000 0x1e000000>/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dts
 sed -i 's/116736k/480m/g' target/linux/mediatek/image/mt7981.mk
-sed -i 's/<0x580000 0x6e80000>/<0x580000 0x1e000000>/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dts
-sed -i 's/114816k/480m/g' target/linux/mediatek/image/mt7981.mk
+sed -i 's/<0x580000 0x6e80000>/<0x580000 0x1cf28000>/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-jcg-q30.dts
+sed -i 's/114816k/474272k/g' target/linux/mediatek/image/mt7981.mk
 
 
